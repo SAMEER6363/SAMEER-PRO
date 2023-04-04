e7987892b0fbc32e993ae6fac749f31055be0e2e
SAMEER HASHMI <116084722+SAMEER6363@users.noreply.github.com>
2023-04-04 18:06:23 +0500

    Update SAMEER.py

--- SAMEER.py
+++ SAMEER.py
@@ -1,4 +1,6 @@
-import os 
+import os, time 
+print("CHAKING UPDATE ");time.sleep(1)
+os.system("git pull ")
 print("FIRST JOIN FACEBOOK GROUP ")
 os.system("xdg-open https://www.facebook.com/groups/147435904593672/?ref=share") 
 import sameer
